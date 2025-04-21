b1f5b2f4c2483f54758291cde0288333792c35a4
Govi-p <33754811+Govi-p@users.noreply.github.com>
2025-04-21 15:23:41 -0400

    Updated If lese changes

--- saveio.py
+++ saveio.py
@@ -1,5 +1,57 @@
-meghna = input()
-if(meghna == "DIED"):
-    print("Surya meets Priya")
+#meghna = input()
+#if(meghna == "DIED"):
+#    print("Surya meets Priya")
+#else:
+#   print("Surya Weds Meghna")
+
+#MINI CALCULATOR
+'''
+num1 = int(input("A:"))
+num2 = int(input("B:"))
+calc = input("OPERATION:ADD/SUB/MUL/DIV? ")
+if(calc == "ADD"):
+    print("Addition ",num1+num2)
+elif(calc == "SUB"):
+    print("Subtraction ",num1-num2)
+elif(calc == "MUL"):
+    print("Multiplication", num1*num2)
+elif(calc == "DIV"):
+    print("Division", num1/num2)
 else:
-    print("Surya Weds Meghna")
+  print("Invalid Operation")
+
+'''
+
+'''
+score = int(input("Score Percentage: "))
+if(score > 70):
+    name = input("Name: ")
+    dept = input("Department: ")
+    location = input("Location: ")
+    print("YOU ARE ELIGIBLE")
+else:
+    print("YPU ARE NOT ELIGIBLE")
+'''
+"""
+salary = int(input("SALARY: "))
+age = int(input("AGE: "))
+if(salary >= 20000 or age <= 25):
+    loan = int(input("LOAN AMOUNT: "))
+    if(loan <= 50000):
+        print("YOU ARE ELIGIBLE FOR LOAN")
+    else:
+        print("MAX LOAN AMOUNT IS 50000")
+else:
+    print("YOPU ARE NOT ELIGIBLE FOR LOAN")
+
+"""
+
+
+
+
+
+
+
+
+
+
